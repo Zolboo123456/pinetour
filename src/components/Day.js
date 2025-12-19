97cdcdd5df22984e294e10000ead73257f547cb1
Zolboo123456 <knownasmisora@gmail.com>
2025-12-18 19:46:22 -0800

    finalized

--- src/components/Day.js
+++ src/components/Day.js
@@ -2,14 +2,11 @@ import "./Day.css";
 
 const Day = (props) => {
   return (
-    <div
-      className="ehniiOdor"
-      style={{ backgroundColor: `${props.colorPath}` }}
-    >
+    <div className="ehniiOdor" style={{ backgroundColor: props.colorPath }}>
       <div>
         <h1>{props.title}</h1>
         <div className="locationDiv">
-          <img className="pin" src="/img/pin.png" />
+          <img className="pin" src="/img/pin.png" alt="pin" />
           <p>{props.location}</p>
         </div>
       </div>
--- src/components/PlacesCards.js
+++ src/components/PlacesCards.js
@@ -5,16 +5,20 @@ const PlacesCard = () => {
   return (
     <div className="cardSectionContainer">
       <div className="cardSection">
-        <Places
-          imagePath="/img/sec2Location1.png"
-          title="Terkhiin Tsagaan Lake"
-        />
-        <Places
-          imagePath="/img/sec2Location2.png"
-          title="Amarbayasgalant Monastry"
-        />
-        <Places imagePath="/img/sec2Location3.png" title="Khusvgul Lake" />
-        <Places imagePath="/img/sec2Location4.png" title="Khorgiin Togoo" />
+        <div className="deed">
+          <Places
+            imagePath="/img/sec2Location1.png"
+            title="Terkhiin Tsagaan Lake"
+          />
+          <Places
+            imagePath="/img/sec2Location2.png"
+            title="Amarbayasgalant Monastry"
+          />
+        </div>
+        <div className="dood">
+          <Places imagePath="/img/sec2Location3.png" title="Khusvgul Lake" />
+          <Places imagePath="/img/sec2Location4.png" title="Khorgiin Togoo" />
+        </div>
       </div>
     </div>
   );
